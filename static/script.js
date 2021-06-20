1e39a11478a9fb39a4116898d9c01b09b47393cb
RacconWebFenix <dfenixweb@gmail.com>
2021-06-20 10:14:26 -0300

    Adicionado funcionalidades e Estetica melhorada

--- static/script.js
+++ static/script.js
@@ -1,41 +1,45 @@
 const cardPersonagem = document.querySelectorAll(".cardPersonagem");
 const radios = document.querySelectorAll("input");
-const botao = document.querySelector("#selecionar");
 const selecionarPersonagens = document.querySelectorAll(
   ".selecionarPersonagens"
 );
-
-const descricao = document.querySelectorAll('.desc' )
+const descricao = document.querySelectorAll(".desc");
 const racaSelecionada = document.querySelectorAll(".cardRacaSelecionar");
-const racaImagem = document.querySelectorAll('.racaImagem')
-const armamento = document.querySelectorAll('.armamentos')
+const racaImagem = document.querySelectorAll(".racaImagem");
+const armamento = document.querySelectorAll(".armamentos");
+const imagemFundos = document.querySelector("#imagemPersFundos");
+const audios = document.querySelectorAll(".audios");
+const mensagemDeDano = document.querySelector("#dano");
+const botaoDano = document.querySelector("#botao");
+const hpPersonagem = document.querySelectorAll(".hpPersonagem");
+
+
+let nomesPersonagem;
+let hpPersonagemSelecionado;
+let nomePersonagemSelecionado;
 
 function selecaoDeRaca() {
   for (let i = 0; i < racaSelecionada.length; i++)
     if (racaSelecionada[i].checked) {
-      
       cardPersonagem[i].style.zIndex = "1";
-      cardPersonagem[i].style.margin = "2rem";
+      cardPersonagem[i].style.margin = "1rem";
       cardPersonagem[i].style.transition = "all 1s";
       cardPersonagem[i].style.opacity = "1";
       racaSelecionada[i].parentElement.style.boxShadow = "0 0 6rem yellow";
+      racaSelecionada[i].parentElement.style.padding = "8px";
       racaSelecionada[i].parentElement.style.borderColor = "";
       racaSelecionada[i].parentElement.style.borderRadius = "0.5rem";
-  
-    
       nomesPersonagem = racaSelecionada[i].value;
     } else {
       racaSelecionada[i].parentElement.style.borderColor = "";
       racaSelecionada[i].parentElement.style.boxShadow = "";
       cardPersonagem[i].style.zIndex = "-1";
-      cardPersonagem[i].style.margin = "-15rem";
+      cardPersonagem[i].style.margin = "-1rem";
+      cardPersonagem[i].style.marginTop = "-25rem";
       cardPersonagem[i].style.transition = "all 1s";
       cardPersonagem[i].style.opacity = "-1";
     }
-  console.log(nomesPersonagem);
 }
-
-
 
 function selecionarRaca() {
   for (let i = 0; i < radios.length; i++) {
@@ -76,11 +80,11 @@ function selecionarRaca() {
 }
 
 function selecao() {
-  let nomesPersonagem = "";
   for (let i = 0; i < selecionarPersonagens.length; i++)
     if (selecionarPersonagens[i].checked) {
+      hpPersonagemSelecionado = parseInt(hpPersonagem[i].innerHTML);
       selecionarPersonagens[i].parentElement.style.transition = "all 1s";
-      descricao[i].style.opacity = '1'
+      descricao[i].style.opacity = "1";
       armamento[i].style.transition = "all 1s";
       armamento[i].style.opacity = "1";
       armamento[i].style.marginTop = "1rem";
@@ -88,6 +92,12 @@ function selecao() {
       selecionarPersonagens[i].parentElement.style.boxShadow =
         "0 0 6rem darkblue";
       nomesPersonagem = selecionarPersonagens[i].value;
+      nomePersonagemSelecionado = selecionarPersonagens[i].value;
+      let persToLower = nomesPersonagem.toLowerCase().replace(" ", "");
+      imagemFundos.setAttribute("src", `/static/img/${persToLower}.png`);
+      imagemFundos.style.maxWidth = "100%";
+      audios[i].setAttribute("src", `/static/audios/${persToLower}.mp3`);
+      audios[i].play();
     } else {
       armamento[i].style.transition = "all 1s";
       armamento[i].style.opacity = "-1";
@@ -95,9 +105,35 @@ function selecao() {
       armamento[i].style.marginTop = "-1rem";
       selecionarPersonagens[i].parentElement.style.borderColor = "";
       selecionarPersonagens[i].parentElement.style.boxShadow = "";
-      descricao[i].style.opacity = '0'
+      descricao[i].style.opacity = "0";
     }
-  console.log(nomesPersonagem);
 }
 
-botao.addEventListener("click", selecao);
+function rolarOsDados() {
+  const min = Math.ceil(80);
+  const max = Math.floor(200);
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+botao.addEventListener("click", () => {
+  selecao();
+  if (
+    !nomesPersonagem ||
+    nomesPersonagem == "Zergs" ||
+    nomesPersonagem == "Protoss" ||
+    nomesPersonagem == "Terranos"
+  ) {
+    alert("Selecione a raça e o personagem para calcular o dano");
+    return;
+  }
+  const danoDados = rolarOsDados();
+  let resultado = "Dano: " + danoDados + "! ";
+
+  if (danoDados >= hpPersonagemSelecionado) {
+    resultado += `Você conseguiu destruir ${nomePersonagemSelecionado} com suas forças!`;
+  } else {
+    resultado += `${nomePersonagemSelecionado} ainda impera nas galaxias, tente novamente!`;
+  }
+ 
+  mensagemDeDano.innerHTML = resultado;
+});
